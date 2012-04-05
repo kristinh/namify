d34e03a3b54d4f8fabee02ac694769a96c5a9127
kristinh <kristinh@precis-project.com>
2012-04-05 23:38:47 +0200

    installed cucumber and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Namify::Application.config.secret_token = 'aeb4eedc8a5b20e53e7e5476e70ec7a91c5697710fa8646ec9afd90f7144842a9e709b1d6ea7f69c3e49bfe35c4b396425ef875a87ef783e2330c6d5fc243040'
