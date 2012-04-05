a22d14ec651a5d5d116a3b7d8a85457612e47abf
kristinh <kristinh@precis-project.com>
2012-04-06 01:06:12 +0200

    changed generator to use haml, rspec and factory girl

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,11 @@ module Namify
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
