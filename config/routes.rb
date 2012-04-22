0679488feda1ebe4d384a42b6fe678640e5e3507
kristinh <kristinh@precis-project.com>
2012-04-22 09:44:14 +0200

    Added static contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Namify::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
+  get "static_pages/contact"
 
   resources :name_lists
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,18 @@ describe "Static pages" do
                     :text => "Namify | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Namify | Contact")
+    end
+  end
 end
